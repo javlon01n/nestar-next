896f9c728e7051191724f5b9a7797dc2f4c5d487
MARK <joxa030@gmail.com>
2026-09-20 20:45:12 +0900

    feat: develop TrendProperties component based on react query data

--- libs/components/homepage/TrendProperties.tsx
+++ libs/components/homepage/TrendProperties.tsx
@@ -7,19 +7,35 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay, Navigation, Pagination } from 'swiper';
 import { Property } from '../../types/property/property';
 import { PropertiesInquiry } from '../../types/property/property.input';
-import TrendPropertyCard from './TrendPropertyCard';
+import TrendPropertyCard from "./TrendPropertyCard";
+import { useQuery } from "@apollo/client";
+import { GET_PROPERTIES } from "../../../apollo/user/query";
+import { T } from "../../types/common";
 
 interface TrendPropertiesProps {
-	initialInput: PropertiesInquiry;
+  initialInput: PropertiesInquiry;
 }
 
 const TrendProperties = (props: TrendPropertiesProps) => {
-	const { initialInput } = props;
-	const device = useDeviceDetect();
-	const [trendProperties, setTrendProperties] = useState<Property[]>([]);
+  const { initialInput } = props;
+  const device = useDeviceDetect();
+  const [trendProperties, setTrendProperties] = useState<Property[]>([]);
 
-	/** APOLLO REQUESTS **/
-	/** HANDLERS **/
+  /** APOLLO REQUESTS **/
+  const {
+    loading: getPropertiesLoading,
+    data: getPropertiesData,
+    error: getPropertiesError,
+    refetch: getPropertiesRefetch,
+  } = useQuery(GET_PROPERTIES, {
+    fetchPolicy: "cache-and-network",
+    variables: { input: initialInput },
+    notifyOnNetworkStatusChange: true,
+    onCompleted: (data: T) => {
+      setTrendProperties(data?.getProperties?.list);
+    },
+  });
+  /** HANDLERS **/
 
 	if (trendProperties) console.log('trendProperties:', trendProperties);
 	if (!trendProperties) return null;
